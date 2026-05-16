93405271c71d8fa6d0f36c0d541d4d9b7f1134ca
gis <giseldice6@gmail.com>
2026-05-15 19:28:42 -0600

    Actividad de Oauth

---
+++ Oauth/build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "9.1.0" apply false
+    id("com.android.library") version "9.1.0" apply false
+    // Si usas Kotlin en el proyecto, añade también estos:
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
